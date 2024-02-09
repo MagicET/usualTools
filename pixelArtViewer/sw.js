5d8fafb2d179b57088762e5e6b8d0110bbd8e3ef
Magicet <60283888+MagicET@users.noreply.github.com>
2024-02-09 15:27:48 +0900

    Create sw.js

---
+++ pixelArtViewer/sw.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = 'pixelArtViewer';
+var urlsToCache = [
+    '/usualTools/pixelArtViewer/favicon_512.PNG'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
